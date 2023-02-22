971a4b894585e03e35884b949ea10c9a04ed526e
osolarprg <osolarprg@gmail.com>
2023-02-22 19:29:18 +0100

    commit inicial :tada:

---
+++ js/app.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Constantes Referenciales
+
+  // ---
+
+  // letiables Generales
+
+  // ---
+
+  // Referencias HTML
+
+  // ---
+
+  // Funciones Generales
+
+  // ---
+
+  // Call-Backs
+
+  // ---
+
+  // Gestores de Eventos
+
+  // ---
+
+  // Inicializar letiables
+  let iniciarVariables = () => {
+    // --
+  };
+
+  // Referenciar HTML
+  let referenciarElementos = () => {
+    //  --
+  };
+
+  // Asignar Eventos
+  let asignarEventos = () => {
+    // --
+  };
+
+  // Lanzar Aplicación
+  let lanzarAplicacion = () => {
+    // --
+  };
+
+  // Iniciar Aplicación
+  let iniciarAplicacion = () => {
+    // Inicializar letiables
+    iniciarVariables();
+
+    // Referenciar Elementos HTML
+    referenciarElementos();
+
+    // Asignar Gestores de Eventos
+    asignarEventos();
+
+    // Lanzar Aplicación
+    lanzarAplicacion();
+  };
+
+  // Ejecutar Código
+  iniciarAplicacion();
+});
